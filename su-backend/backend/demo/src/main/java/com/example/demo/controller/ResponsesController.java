569b70f4ba3cff5587bbadd39d98a67777db5f18
Daria Sevostianova <d.sevostianova@innopolis.university>
2026-06-30 23:54:05 +0300

    Finished up 'responses' backend

--- su-backend/backend/demo/src/main/java/com/example/demo/controller/ResponsesController.java
+++ su-backend/backend/demo/src/main/java/com/example/demo/controller/ResponsesController.java
@@ -2,19 +2,17 @@ package com.example.demo.controller;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.entity.Responses;
 import com.example.demo.repository.ResponsesRepository;
-
 import java.util.Optional;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 public class ResponsesController {
     private final ResponsesRepository repository;
+
     public ResponsesController (ResponsesRepository repository) {
         this.repository = repository;
     }
-
     @GetMapping("/responses/{id}")
     public Optional<Responses> getMethodName(@PathVariable Long id) {
         return repository.findById(id);
