e36f5a760ca83d42565330a47b291d3a9a8cdeea
Daria Sevostianova <d.sevostianova@innopolis.university>
2026-07-17 13:54:47 +0300

    PUT endpoint for events

--- su-backend/backend/demo/src/main/java/com/example/demo/events/controller/EventsController.java
+++ su-backend/backend/demo/src/main/java/com/example/demo/events/controller/EventsController.java
@@ -4,10 +4,12 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.events.repository.EventsRepository;
 import com.example.demo.events.entity.Events;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
@@ -32,5 +34,23 @@ public class EventsController {
     @DeleteMapping("/event/{id}")
     public void deleteEvent(@PathVariable Long id) {
         repository.deleteById(id);
+    }
+
+    @PutMapping("event/{id}")
+    public Events editEvent(@PathVariable Long id, @RequestBody Events entity) {
+        Optional<Events> optional = repository.findById(id);
+        if (optional.isPresent()) {
+            Events event = optional.get();
+            event.setCreatedAt(entity.getCreatedAt());
+            event.setDescription(entity.getDescription());
+            event.setEventLocation(entity.getEventLocation());
+            event.setEventTime(entity.getEventTime());
+            event.setFinishedAt(entity.getFinishedAt());
+            event.setGalleryUrl(entity.getGalleryUrl());
+            event.setPhotoUrls(entity.getPhotoUrls());
+            event.setTitle(entity.getTitle());
+            return repository.save(event);
+        }
+        return null;
     }
 }
